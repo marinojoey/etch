b09a27bdce5460973bde3bd7e6695e7b5f67e9eb
Joey Marino <joeymarino94@gmail.com>
2021-09-16 13:43:19 -0500

    made grid. Added css

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('#container')
+for (i=0; i<256; i++) {
+    const div = document.createElement('div')
+    div.classList.add('square')
+    container.appendChild(div)
+}
+
+
+
+
