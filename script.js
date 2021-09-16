60e547865c44af89d29e3d950cc721c4314cf486
Joey Marino <joeymarino94@gmail.com>
2021-09-16 17:24:44 -0500

    made big changes to ui and functionality. calling it a day.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container = document.querySelector('#container')
+
+container.addEventListener('mouseover', e => {
+    e.target.style.backgroundColor = "rgb(97, 7, 30)"
+    }
+);
 for (i=0; i<256; i++) {
     const div = document.createElement('div')
     div.classList.add('square')
     container.appendChild(div)
 }
-
-
 
 
