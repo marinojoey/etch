bdc2368ae42767fc3eeb63601c5a12d46975b1ae
Joey Marino <joeymarino94@gmail.com>
2021-09-17 11:39:39 -0500

    First working product. Missing grid resize on reset and dynamic pen coloring

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const container = document.querySelector('#container')
+const resetEl = document.querySelector(".reset")
 
 container.addEventListener('mouseover', e => {
     e.target.style.backgroundColor = "rgb(97, 7, 30)"
     }
 );
+
 for (i=0; i<256; i++) {
     const div = document.createElement('div')
     div.classList.add('square')
     container.appendChild(div)
+    resetEl.addEventListener('click', e => {
+        div.style.backgroundColor = 'rgb(7, 110, 110)'
+        }
+    )
 }
-
-
